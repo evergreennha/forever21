6a5a8d8cfab6905edb46adb64e86b6b5da9707f2
Nguyễn Hải Anh <evergreennha@gmail.com>
2021-02-05 11:18:35 +0700

    Login view

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react'
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,7 +14,10 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 
 import {
   Header,
@@ -24,91 +27,79 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+const App = () => {
+  const [email, setEmail] = useState()
+  const [toggleCheckBox, setToggleCheckBox] = useState(false)
+  
+  const onChangeText = (value) => setEmail(value)
+  return(
+    <View style={{paddingTop: 40, flex: 1}}>
+      <View style={{paddingHorizontal: 20}}>
+        <View>
+          <Text style={{color: 'grey'}}>EMAIL ADDRESS</Text>
+          <TextInput
+            style = {{height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={text => onChangeText(text)}
+            value={email}
+            placeholder='Type your email'
+
+          />
+          <Text style={{color:'red',fontSize: 17}}>Require field</Text>  
+        </View> 
+        <View style={{marginTop: 20}}>
+          <Text style = {{color: 'grey'}}>PASSWORD</Text>
+          <View>
+            <TextInput
+              style={{height:40, borderColor: 'gray',borderWidth:1}}
+              onChangeText={text => onChangeText(text)}
+              value={email}
+              placeholder='Type your password'
+            />
+            <Text style={{position:'absolute',top: 11, right: 10}}>Show</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        </View>
+
+        <View style={{flexDirection:'row',alignItems: 'center'}}>
+          <CheckBox
+            disabled = {false}
+            value={toggleCheckBox}
+            onValueChange={(newValue) => setToggleCheckBox(newValue) }
+          />
+          <Text>Remember me</Text>
+        </View>
+        <TouchableOpacity style={{
+          backgroundColor: 'yellow',
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 40
+        }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>SIGN IN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={{marginTop:30,textAlign:'center',textDecorationLine:'underline'}}>Forgot password?</Text>
+        </TouchableOpacity>
+
+        <View style={{borderTopWidth:1,marginTop:30,borderColor:'grey'}}></View>
+
+        <TouchableOpacity>
+          <Text style={{marginTop: 30,textAlign:'center',textDecorationLine:'underline'}}>Don't have an account?</Text>
+        </TouchableOpacity>
+
+      </View>
+      <View style={{backgroundColor:'#e3e2da',padding:20,position:'absolute',bottom:0,width:'100%'}}>
+        <Text>Face ID setting</Text>
+        <Text>Turn on/off face ID, go to [My account] > [Setting] > [Face ID] > On/Off</Text>
+      </View>
+    </View>
   );
 };
+const styles =  StyleSheet.create({
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
 });
+  
+
+
 
 export default App;
